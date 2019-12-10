84a29f220a168bbee9571e2d05ede5efe417a222
Wouter van de Molengraft <wouter@softmedia.nl>
2019-12-10 13:55:59 +0100

    added script for purging subtitle cache

---
+++ src/bin/commands/purge-subtitle-cache.js
@@ -0,0 +1,24 @@
+import config from '@/config';
+import sywac from 'sywac';
+import axios from 'axios';
+import { Bar } from 'cli-progress';
+
+sywac.command('purge-subtitle-cache <assetId>', async ({ assetId }) => {
+  const entries = await config.destinationFileSystem.list(`/${assetId}/subtitles`);
+  const bar = new Bar();
+
+  bar.start(entries.length);
+  let i =0;
+  for(let { name } of entries) {
+    await axios.post('https://bunnycdn.com/api/purge', null, {
+      params: {
+        url: `https://${config.bunnyCDN.hostname}.b-cdn.net/${assetId}/subtitles/${name}`
+      },
+      headers: {
+        AccessKey: config.bunnyCDN.authenticationKey
+      }
+    });
+    bar.update(++i);
+  }
+  bar.stop();
+});
