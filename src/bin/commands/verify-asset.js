532dbcf1423c79509b5db0d685e77a8250d6be03
Wouter van de Molengraft <wouter@softmedia.nl>
2020-01-27 11:12:59 +0100

    added option for counting files only for verify-asset command

--- src/bin/commands/verify-asset.js
+++ src/bin/commands/verify-asset.js
@@ -40,52 +40,63 @@ const getDuration = async source => {
   return parseFloat(_.get(json, 'format.duration'));
 };
 
-sywac.command('verify-asset <assetId>', async ({ assetId }) => {
-  const asset = await Asset.findById(assetId);
+sywac.command('verify-asset <assetId>', {
+  setup(sywac) {
+    sywac
+      .boolean('-c, --count-only', {
+        desc: 'Only verify file counts'
+      })
+  },
 
-  if (!asset) {
-    throw 'Item not found';
-  }
+  async run({ assetId, ...opts }) {
+    const asset = await Asset.findById(assetId);
 
-  if (asset.bitrates.length !== (asset.numStreams || asset.jobs.length)) {
-    console.error(`Not all jobs are completed: ${_.difference(_.map(asset.jobs, 'maxrate'),
-      asset.bitrates).join(', ')} are missing`);
-    process.exit(1);
-  }
-
-  console.info('Verifying file count');
-
-  const entries = await config.destinationFileSystem.list(`/${assetId}`);
-  const counts = _.countBy(entries, ({name}) => name.split('.')[1]);
-
-  const max = _.max(_.values(counts));
-
-  const faulty = _.keys(_.omit(counts, ['m3u8', 'undefined']))
-  .filter(bitrate => Math.abs(counts[bitrate] - max) >= 5);
-  if (faulty.length > 0) {
-    console.warn(`File count for bitrates ${faulty.join(', ')} differ from maximum.`);
-  }
-  asset.bitrates = _.difference(asset.bitrates, faulty);
-
-  const good = [];
-  // verify durations of all bitrates
-  for (let entry of [...asset.streams, ...asset.audioStreams]) {
-    const timestamp = moment().add(8, 'hours').valueOf();
-    const signature = computeSignature(assetId, timestamp);
-    const videoUrl = `${config.base}/streams/${timestamp}/${signature}/${entry.filename}`;
-    console.info(`Checking duration for ${entry.bitrate || `${entry.bandwidth/1024}k`}`);
-    const duration = await getDuration(videoUrl);
-    if (Math.abs(asset.videoParameters.duration - Math.floor(duration)) > 2) {
-      console.error(`Duration for bitrate ${entry.bitrate} (${duration}) differs from source (${asset.videoParameters.duration}).`);
-    } else {
-      good.push(entry.bitrate);
+    if (!asset) {
+      throw 'Item not found';
     }
-  }
-  asset.bitrates = good;
 
-  await asset.save();
+    if (asset.bitrates.length !== (asset.numStreams || asset.jobs.length)) {
+      console.error(`Not all jobs are completed: ${_.difference(_.map(asset.jobs, 'maxrate'),
+        asset.bitrates).join(', ')} are missing`);
+      process.exit(1);
+    }
 
-  if (asset.bitrates.length === asset.jobs.length) {
-    console.info('Asset has been verified successfully');
+    console.info('Verifying file count');
+
+    const entries = await config.destinationFileSystem.list(`/${assetId}`);
+    const counts = _.countBy(entries, ({ name }) => name.split('.')[1]);
+
+    const max = _.max(_.values(counts));
+
+    const faulty = _.keys(_.omit(counts, ['m3u8', 'undefined']))
+    .filter(bitrate => Math.abs(counts[bitrate] - max) >= 5);
+    if (faulty.length > 0) {
+      console.warn(`File count for bitrates ${faulty.join(', ')} differ from maximum.`);
+    }
+    asset.bitrates = _.difference(asset.bitrates, faulty);
+
+    if (!opts['count-only']) {
+      const good = [];
+      // verify durations of all bitrates
+      for (let entry of [...asset.streams, ...asset.audioStreams]) {
+        const timestamp = moment().add(8, 'hours').valueOf();
+        const signature = computeSignature(assetId, timestamp);
+        const videoUrl = `${config.base}/streams/${timestamp}/${signature}/${entry.filename}`;
+        console.info(`Checking duration for ${entry.bitrate || `${entry.bandwidth / 1024}k`}`);
+        const duration = await getDuration(videoUrl);
+        if (Math.abs(asset.videoParameters.duration - Math.floor(duration)) > 2) {
+          console.error(`Duration for bitrate ${entry.bitrate} (${duration}) differs from source (${asset.videoParameters.duration}).`);
+        } else {
+          good.push(entry.bitrate);
+        }
+      }
+      asset.bitrates = good;
+    }
+
+    await asset.save();
+
+    if (asset.bitrates.length === asset.jobs.length) {
+      console.info('Asset has been verified successfully');
+    }
   }
 });
