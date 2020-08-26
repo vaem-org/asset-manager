c5d32a577b40913fcfd1d931d8023143e8d7cc1a
wouter <wouter@cinetree.nl>
2020-08-26 17:04:12 +0200

    Use configurable Azure location for instances

--- src/config.js
+++ src/config.js
@@ -109,7 +109,8 @@ const config = merge({
     subscriptionId: process.env.AZURE_SUBSCRIPTION_ID,
     storageAccountName: process.env.AZURE_STORAGE_ACCOUNT_NAME,
     storageAccountKey: process.env.AZURE_STORAGE_ACCOUNT_KEY,
-    shareName: process.env.AZURE_SHARE_NAME
+    shareName: process.env.AZURE_SHARE_NAME,
+    location: process.env.AZURE_LOCATION || 'WestEurope'
   },
 
   encoderToken: process.env.ENCODER_TOKEN || randomBytes(16).toString('hex'),
