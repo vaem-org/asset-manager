1189785130d941c518d857207387412e70372f93
Wouter van de Molengraft <wouter@softmedia.nl>
2020-11-20 14:57:28 +0100

    added script for completion

--- src/bin/commands/complete.js
+++ src/bin/commands/complete.js
@@ -17,8 +17,17 @@
  */
 
 import sywac from 'sywac';
+import { dirname } from 'path';
 import { Asset } from '@/model/asset';
-import masterPlaylist from '@/lib/master-playlist'; sywac;
+import masterPlaylist from '@/lib/master-playlist';
+import config from '@/config';
+import { getStreamInfo } from '@/lib/stream';
+import _ from 'lodash';
+import { promisify } from "util";
+import { execFile as _execFile } from "child_process";
+import { verifyAsset } from '@/lib/verify-asset';
+
+const execFile = promisify(_execFile);
 
 sywac.command('complete <assetId>', async ({ assetId }) => {
   const asset = await Asset.findById(assetId);
@@ -27,8 +36,61 @@ sywac.command('complete <assetId>', async ({ assetId }) => {
     throw 'Item not found';
   }
 
+  const list = (await config.destinationFileSystem.list(assetId))
+    .map(({ name }) => name)
+    .filter(name => name.endsWith('k.m3u8'))
+  ;
+
+  asset.bitrates = list.map(name => name.split('.')[1]);
+
+  asset.streams = [];
+  // ffprobe first bitrate
+  const source = await getStreamInfo(assetId, '127.0.0.1');
+
+  const base = dirname(source.streamUrl);
+  for(let filename of list) {
+    let stream;
+    let stdout;
+    try {
+      ({ stdout } = await execFile('ffprobe', [
+        '-v', 'error',
+        '-print_format', 'json',
+        '-show_format',
+        '-show_streams',
+        `http://127.0.0.1:${config.port}${base}/${filename}`
+      ]));
+
+      stream = _.find(_.get(JSON.parse(stdout), 'streams', []), { codec_type: 'video' });
+    }
+    catch (e) {
+      throw `ffprobe failed with ${e.stderr || e.toString()}`
+    }
+
+    if (stream) {
+      const aspect = _.get(stream, 'display_aspect_ratio', '')
+        .split(':')
+        .map(value => parseInt(value))
+        .filter(value => value)
+      ;
+
+      asset.streams.push(
+        {
+          filename,
+          bandwidth: parseInt(filename.split('.')[1].replace(/k$/, '')) * 1024,
+          resolution:
+            aspect.length > 0 ? Math.max(stream.width,
+              Math.floor(stream.height / aspect[1] * aspect[0])) + 'x' + stream.height :
+              `${stream.width}x${stream.height}`
+          ,
+          codec: 'avc1.640029'
+        });
+    }
+  }
+  await asset.save();
   await masterPlaylist(asset._id);
 
-  asset.state = 'processed';
+  await verifyAsset({
+    assetId
+  });
   await asset.save();
 });
